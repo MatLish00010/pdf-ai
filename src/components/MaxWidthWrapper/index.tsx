f23fe6d8050d876ecc8800aa95b3c68eec75e58c
NEAmico <lishay.matvey20001506@gmail.com>
2023-10-08 18:19:22 +0200

    update tailwind config

---
+++ src/components/MaxWidthWrapper/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import {cn} from '@/app/lib/utils';
+
+type Props = {
+  className?: string;
+  children: React.ReactNode;
+};
+
+export default function MaxWidthWrapper({className, children}: Props) {
+  return (
+    <div className={cn('mx-auto w-full max-w-screen-xl px-2.5 mb:px-20', className)}>
+      {children}
+    </div>
+  );
+}
