cc0cf4c42aad98a8d016d66caab0b6a2361c20a3
NEAmico <lishay.matvey20001506@gmail.com>
2023-10-09 17:12:32 +0200

    add auth callback

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -1,8 +1,20 @@
 import {useRouter, useSearchParams} from 'next/navigation';
 
+import {trpc} from '@/app/_trpc/client';
+
 export default function Page() {
   const router = useRouter();
 
   const searchParams = useSearchParams();
   const origin = searchParams.get('origin ');
+
+  const {data, isLoading} = trpc.authCallback.useQuery(undefined, {
+    onSuccess: ({success}) => {
+      console.log('lel');
+      if (success) {
+        // user is asynced
+        router.push(origin ? `/${origin}` : '/dashboard');
+      }
+    },
+  });
 }
--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -11,6 +11,10 @@ export const appRouter = router({
     if (!user.id || !user.email) throw new TRPCError({code: 'UNAUTHORIZED'});
 
     // check in DB
+
+    return {
+      success: true,
+    };
   }),
 });
 
